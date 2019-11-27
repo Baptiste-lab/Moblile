9ac5a9d2eafb642a006742ea6bcffa2f9c875aff
unknown <Baptiste>
2019-11-27 22:23:04 +0200

    Mywork

--- AppAcc/settings.py
+++ AppAcc/settings.py
@@ -118,9 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
