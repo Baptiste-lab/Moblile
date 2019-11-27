75f5625e981267a2d56d5f1750b81a8e92ddb519
unknown <Baptiste>
2019-11-28 01:26:50 +0200

    mywork

--- AppAcc/settings.py
+++ AppAcc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 
 
@@ -77,10 +78,7 @@ WSGI_APPLICATION = 'AppAcc.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
